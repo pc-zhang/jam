511588424793ca0396530fa5a323dd17181633ed
zhangpengcheng-iri <zhangpengcheng-iri@360.cn>
2017-04-29 20:18:08 +0800

    correct

--- 2017round1B_A.cpp
+++ 2017round1B_A.cpp
@@ -1,6 +1,6 @@
 // test.cpp : 定义控制台应用程序的入口点。
 //
-#include <boost/lambda/lambda.hpp>
+#include <boost/foreach.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
@@ -14,28 +14,44 @@
 #include <cassert>
 using namespace std;
 
-string compute(int n, int r, int o, int y, int g, int b, int v) {
-  assert(r+o+y+g+b+v==n);
-  //assert(0==o && 0==g && 0==v);
-  if (0 == o && 0 == g && 0 == v) {
+typedef struct {
+  int K;
+  int S;
+}Horse;
 
+string compute(int D, int N, const vector<Horse> &horses) {
+  assert(N == horses.size());
+  double max_time = 0;
+
+  BOOST_FOREACH(Horse h, horses) {
+    double time = 1.0*(D - h.K) / h.S;
+    if (time > max_time) {
+      max_time = time;
+    }
   }
 
+  double max_speed = D / max_time;
 
-  return "IMPOSSIBLE";
+  return to_string(max_speed);
 }
 
 int main() {
-  ifstream in("samples.txt");
+  ifstream in("A-large-practice.in");
   ofstream out("out.txt");
 
   int T;
   in >> T;
   for (int i = 1; i <= T; ++i) {
-    int n, r, o, y, g, b, v;
+    int D, N;
 
-    in >> n >> r >> o >> y >> g >> b >> v;
-    out << "Case #" << i << ": " << compute(n, r, o, y, g, b, v) << endl;
+    in >> D >> N;
+    vector<Horse> horses;
+    for (int j = 0; j < N; j++) {
+      Horse h;
+      in >> h.K >> h.S;
+      horses.push_back(h);
+    }
+    out << "Case #" << i << ": " << compute(D,N,horses) << endl;
   }
 
   return 0;
